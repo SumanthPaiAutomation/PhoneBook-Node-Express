dd9e0d2064f8c37fec4b29b7c16868a0505cf6e4
Sumanth Pai <52402733+SumanthPaiJS@users.noreply.github.com>
2019-09-05 20:28:54 +0530

    Add files via upload

---
+++ Server/models/users.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength:8,
+    maxlength:28,
+  },
+  phonenumber:{
+    type: Number,
+    unique: true,
+    required:true,
+    minlength:10,
+    maxlength:10,
+
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+ 
+});
+
+userSchema.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    const hashed = await bcrypt.hash(this.password, 10);
+    this.password = hashed;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function(attempt, next) {
+  try {
+    return await bcrypt.compare(attempt, this.password);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = mongoose.model('User', userSchema);
